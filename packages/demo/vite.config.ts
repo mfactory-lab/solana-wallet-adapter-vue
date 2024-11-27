bff9a456e2069bb337dd69929403a98a41858274
tiamo <vk.tiamo@gmail.com>
2024-11-27 05:23:47 +0200

    fix(ci): fix base path

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -1,21 +1,26 @@
 import { resolve } from 'node:path'
+import process from 'node:process'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // https://github.com/davidmyersdev/vite-plugin-node-polyfills
-    nodePolyfills(),
-  ],
-  resolve: {
-    alias: {
-      '~/': `${resolve(__dirname, 'src')}/`,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [
+      vue(),
+      // https://github.com/davidmyersdev/vite-plugin-node-polyfills
+      nodePolyfills(),
+    ],
+    resolve: {
+      alias: {
+        '~/': `${resolve(__dirname, 'src')}/`,
+      },
     },
-  },
-  define: {
-    'process.env': {},
-  },
+    define: {
+      'process.env': {},
+    },
+  }
 })
